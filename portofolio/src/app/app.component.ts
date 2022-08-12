2155c2652c3cbaafdbedd5f7215017b46abe7c9e
Arthur-geek <kofrathur@gmail.com>
2022-08-12 17:07:34 +0000

    particles

--- portofolio/src/app/app.component.ts
+++ portofolio/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portofolio';
+
+  loadScript(url:any) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+  ngOnInit(){
+    this.loadScript('../assets/js/particles.min.js');
+    this.loadScript('../assets/js/app.js');
+  }
 }
